ee03bba9602bad8c2ac017243e1cef9d66128757
Văn Minh Hoàng <hoangvm@biso24.com>
2025-01-04 16:07:09 +0700

    error 1

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl'
 
 const Error = ({ reset }: { reset: () => void }) => {
   const t = useTranslations('error')
+  console.log('error 1')
 
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 md:p-12 dark:border-neutral-800 dark:bg-black">
