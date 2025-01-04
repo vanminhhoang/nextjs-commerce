d1c979c49015386589d390f59660a3448427871c
Văn Minh Hoàng <hoangvm@biso24.com>
2025-01-04 16:08:31 +0700

    error 4

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -8,7 +8,7 @@ const Error = ({ reset }: { reset: () => void }) => {
 
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 md:p-12 dark:border-neutral-800 dark:bg-black">
-      <h2 className="text-xl font-bold">Oh no!</h2>
+      <h2 className="text-xl font-bold">Oh no! 44</h2>
       <p className="my-2">{t('message')}</p>
       <button
         className="mx-auto mt-4 flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white hover:opacity-90"
